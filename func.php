9172ae4125cb95943fea2fd1487b1e97c2221a23
f0s3 <power.aerozoll@gmail.com>
2018-09-08 12:11:21 +0300

    refactor code

--- func.php
+++ func.php
@@ -11,10 +11,9 @@ if (!$sql) {
 }
 mysqli_set_charset($sql, "utf8");
 
-
-function news($sql){
+function news($sql) {
 	$result = mysqli_query($sql, "SELECT * from news ORDER BY -ID");
-	while($row = mysqli_fetch_row($result)){
+	while($row = mysqli_fetch_row($result)) {
 		$date = implode('.', array_reverse(explode('-', $row[2])));
 		echo "<div class='news'>
 				<h1 class='news'>$row[1]</h1>
@@ -29,7 +28,7 @@ function news($sql){
 }
 
 
-function Inews($sql, $a, $x=1){
+function Inews($sql, $a, $x=1) {
 	switch ($x) {
 		case '1':
 			$result = mysqli_query($sql, "SELECT name from news");
@@ -53,26 +52,25 @@ function Inews($sql, $a, $x=1){
 }
 
 
-function sd($x, $a){
+function sd($dayOrMonth, $a) {
 	$Inews = Inews($sql, $a, 0);
 
-	switch ($x) {
+	switch ($dayOrMonth) {
 		case 'day':
-			for ($i=1; $i <= 31 ; $i++) { 
+			for ($i=1;$i <= 31;$i++) { 
 				if ($Inews[1][2] == $i) {
 					echo "<option onClick='blurt(this)' selected>".$i."</option>";
-				}else{
+				} else {
 					echo "<option onClick='blurt(this)'>".$i."</option>";
 				}
 			}
 			break;
-		
 		case 'month':
 			$month = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
-			for ($i=1; $i <= 12 ; $i++) { 
+			for ($i=1;$i <= 12;$i++) { 
 				if ($Inews[1][1] == $i) {
 					echo "<option onClick='blurt(this)' selected>".$month[$i-1]."</option>";
-				}else{
+				} else {
 					echo "<option onClick='blurt(this)'>".$month[$i-1]."</option>";
 				}
 			}
@@ -97,43 +95,54 @@ function homework($sql, $day, $id, $t=1){
 	}		
 }
 
-function timetable($sql, $d, $mode=0) { //mode - режим. Если 0 тогда просто отображение, если 1 тогда текст меняем на инпут
-	if ($d==1) {$_day=1; $_Day=3;}	 //Розділяем таблицю на 2 часті: l and r
-	else {$_day=4; $_Day=5;}			// $_day - с какого дня начинаем; $_Day - каким днем заканчиваем
-	for ($day=$_day; $day<=$_Day; $day++) {
+/* function timetable($sql, $d, $mode = 0)
+$sql: connection to db //todo: needs to be ramoved as it's redundant here
+$d: ?
+$mode: if mode is equal to 0 then display everything in default mode(text), otherwise allow editing(input)
+ 
+ 
+*/
+function timetable($sql, $d, $mode = 0) {
+	if ($d == 1) {
+		$startingDay = 1;
+		$endingDay=3;
+	} else {
+		$startingDay=4;
+		$endingDay=5;
+	}			// $_day - с какого дня начинаем; $_Day - каким днем заканчиваем
+	for ($currentDay = $startingDay;$currentDay <= $endingDay; $currentDay++) {
 		echo "<div class='table'><table>";
-		$result = mysqli_query($sql, "SELECT * FROM `timetable` where id=$day"); //Витягуєм все с timetable
+		$result = mysqli_query($sql, "SELECT * FROM `timetable` where id=$day");
 		$lesson = mysqli_fetch_row($result);
 		mysqli_free_result($result);
-
-		$result = mysqli_query($sql, "SELECT * FROM `homework` WHERE id=$day"); //Витягуєм все с homework
+		$result = mysqli_query($sql, "SELECT * FROM `homework` WHERE id=$day");
 		$homework = mysqli_fetch_row($result);
 		mysqli_free_result($result);
-		for ($i=0; $i <= 8; $i++) {
-			if ($i==8) $last = ['nl', 'bl', 'cl'];
+		for ($i = 0;$i <= 8;$i++) {
+			if ($i == 8) $last = ['nl', 'bl', 'cl'];
 			else $last = [' ', ' ', ' '];
-			if ($i==0) echo "<tr><td class='date' colspan='3'>$lesson[1]</td></tr>";
+			if ($i == 0) echo "<tr><td class='date' colspan='3'>$lesson[1]</td></tr>";
 			else {
 				$less = explode("<^>", $lesson[$i + 1]); //Проверка, урок поділено на 2 групи (например Англ. Мов.№1<^>Англ. Мов.№2) или нет
-				$h = explode("<^>", $homework[$i + 1]); //Проверка дз
-				if ($mode==1){
+				$homeworkCheck = explode("<^>", $homework[$i + 1]);
+				if ($mode == 1) {
 					$less[0] = less($sql, $less[0], $day, $i); // + select
 					$h[0] = "<input type='text' name='".$day.$i."' class='homework' value='".$h[0]."'>";  // + input
 				}
 				echo "<tr>
-				<td class='".$last[0]." n'>$i.</td>
-				<td class='".$last[1]." b'>$less[0]</td>
-				<td class='".$last[2]." c'>$h[0]</td>
+					<td class='".$last[0]." n'>$i.</td>
+					<td class='".$last[1]." b'>$less[0]</td>
+					<td class='".$last[2]." c'>$h[0]</td>
 				</tr>";
-				if (count($less)==2) {
-					if ($mode==1) {
+				if (count($less) == 2) {
+					if ($mode == 1) {
 						$less[1] = less($sql, $less[1], $day, $i, 1); // + selcet
 						$h[1] = "<input type='text' name='".$day.$i."1' class='homework' value='".$h[1]."'>";  // + input
 					}
 					echo "<tr>
-					<td class='".$last[0]." n'></td>
-					<td class='".$last[1]." b'>$less[1]</td>
-					<td class='".$last[2]." c'>$h[1]</td>
+						<td class='".$last[0]." n'></td>
+						<td class='".$last[1]." b'>$less[1]</td>
+						<td class='".$last[2]." c'>$h[1]</td>
 					</tr>";
 				}
 			}
@@ -142,14 +151,14 @@ function timetable($sql, $d, $mode=0) { //mode - режим. Если 0 тогд
 	}
 }
 
-function less($sql, $lesson, $day, $i, $x=0){
+function less($sql, $lesson, $day, $i, $x=0) {
 	$str = "<div class='form_sel'><select name='lesson".$day.$i.$x."' class='select'>";
 
 	$result = mysqli_query($sql, "SELECT name from lessons order by name");
 	while($row = mysqli_fetch_row($result)) {
 		$row = explode("<^>", $row[0]);
 
-		if (count($row)==2) $b = $x;
+		if (count($row) == 2) $b = $x;
 		else $b = 0;
 
 		if ($lesson == $row[$x]) $str .= '<option onClick="blurt(this)" selected>'.$row[$b].'</option>';
@@ -159,7 +168,7 @@ function less($sql, $lesson, $day, $i, $x=0){
 	return $str;
 }
 
-function teachs($sql){
+function teachs($sql) {
 	$result = mysqli_query($sql, "SELECT * from teachers order by tech");
 	while ($row = mysqli_fetch_row($result)) {			
 		echo "<div class='tdiv'>
