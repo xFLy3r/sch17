ee9a9e523b5fd89ac98bc6c19e257a44d4c2e204
Berkutik <you@example.com>
2018-10-04 15:49:08 +0300

    create simlpe XMLHttpRequest

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ function removeAd() {
 		document.getElementsByClassName("cumf_bt_form_wrapper")[0].remove();
 	}, 50);
 }
+
+var cont = "index";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "processor.php?content=" + cont);
+xhr.send();
+
+xhr.onreadystatechange = function() {
+	if (xhr.readyState != 4) return;
+
+	if (xhr.status != 200)
+		//var response = JSON.parse(xhr.responseText);
+		document.writeln(xhr.status + ": " + xhr.statusText);
+	else
+		document.getElementById("main").innerHTML = xhr.responseText;
+}
