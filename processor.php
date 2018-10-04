8d2cb68d3ade33598311fedeff21baf214635962
Berkutik <you@example.com>
2018-10-04 15:43:07 +0300

    create simple processor for XMLHttpRequest

--- processor.php
+++ processor.php
@@ -1,4 +1,17 @@
 <?
+$content = $_GET['content'];
 
-
+switch ($content){
+	
+	case "index":
+		echo "<div class='news'>
+			<h1 class='news'>ОНО ЖИВОЕ</h1>
+			<p class='date'>РАБОТАЕТ</p>
+			<div class='news_text'>
+				<p>НИФИГАСЕ</p>
+			</div>
+			<div class='break'></div>
+			</div>";
+		break;
+}
 ?>
