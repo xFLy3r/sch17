e195ef1cd5b0bad9f953a5637f3c1cb30d1680fb
Berkutik <you@example.com>
2018-10-04 17:59:39 +0300

    minor code edits

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
+refreshContent();
 window.onload = removeAd;
 
 function removeAd() {
 	body.removeChild(document.querySelector(".cbalink"));
 	body.removeChild(document.querySelector(".cumf_bt_form_wrapper"));
-	setTimeout(function() {
-		document.getElementsByClassName("cbalink")[0].remove();
-		document.getElementsByClassName("cumf_bt_form_wrapper")[0].remove();
-	}, 50);
 }
 
-var cont = "index";
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "processor.php?content=" + cont);
-xhr.send();
+function refreshContent(content) {
+	content = content || "index";
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "processor.php?content=" + content);
+	xhr.send();
 
-xhr.onreadystatechange = function() {
-	if (xhr.readyState != 4) return;
+	xhr.onreadystatechange = function() {
+		history.pushState(null, null, "");
+		if (xhr.readyState != 4) return;
 
-	if (xhr.status != 200)
-		//var response = JSON.parse(xhr.responseText);
-		document.writeln(xhr.status + ": " + xhr.statusText);
-	else
-		document.getElementById("main").innerHTML = xhr.responseText;
+		if (xhr.status != 200)
+			document.writeln(xhr.status + ": " + xhr.statusText);
+		else
+			document.getElementById("main").innerHTML = xhr.responseText;
+		
+		document.getElementsByClassName("cbalink")[0].remove();
+		document.getElementsByClassName("cumf_bt_form_wrapper")[0].remove();
+	}
 }
