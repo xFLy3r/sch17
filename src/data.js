6d03bda6375a11be0d9c4e9eb81e2e9dbf0c218f
f0s3 <power.aerozoll@gmail.com>
2018-12-15 01:26:00 +0200

    Add sample json

---
+++ src/data.js
@@ -0,0 +1,28 @@
+module.exports = {
+    news: [],
+    schedule: [],
+    teachers: [
+        { name: "Тихоненко Аліна Миколаївна", subject: "Інформатика №1" },
+        { name: "Саражинський Микола Олександрович", subject: "Інформатика№2" },
+        { name: "Сандецька Ірина Федорівна", subject: "" },
+        { name: "Линик Олена Петрівна", subject: "" },
+        { name: "Куцевська Інна Вікторівна", subject: "" },
+        { name: "Єрмоленко Лариса Анатоліївна", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" },
+        { name: "", subject: "" }
+    ],
+    books: []
+}
