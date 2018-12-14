950f5057dac6aa98dcddde6023846bdb59aa272d
f0s3 <power.aerozoll@gmail.com>
2018-12-15 01:26:37 +0200

    Use data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const app = require('express')();
+const data = require('data');
 
 app.get('/', (req, res) => {
-    res.send({name: 'message'});
+    res.send({news: data['news']});
 });
 
 app.listen(4201, () => {console.log('application is running on port 4201.')});
