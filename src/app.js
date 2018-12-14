9a08d7ad14ed90db42fb717a49c358285d7a8791
f0s3 <power.aerozoll@gmail.com>
2018-12-15 01:45:55 +0200

    Add GET requests

--- src/app.js
+++ src/app.js
@@ -5,4 +5,20 @@ app.get('/', (req, res) => {
     res.send({news: data['news']});
 });
 
-app.listen(4201, () => {console.log('application is running on port 4201.')});
+app.get('/news', (req, res) => {
+    res.send({news: data['news']});
+});
+
+app.get('/schedule', (req, res) => {
+    res.send({news: data['schedule']});
+});
+
+app.get('/teachers', (req, res) => {
+    res.send({news: data['teachers']});
+});
+
+app.get('/books', (req, res) => {
+    res.send({news: data['books']});
+});
+
+app.listen(4201, () => { console.log('application is running on port 4201.') });
