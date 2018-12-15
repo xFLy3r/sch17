1a59e23d0f8af24cdc6b6ba12b72a67842f80862
f0s3 <power.aerozoll@gmail.com>
2018-12-15 02:25:18 +0200

    Add POST /news request

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const data = require('./data');
 
-class Article {
-    constructor(title, text) {
-        this.title = title;
-        this.text = text;
-        this.date = Date();
-    }
-}
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send({news: data['news']});
@@ -15,6 +11,15 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
     res.send({news: data['news']});
+});
+
+app.post('/news', (req, res) => {
+    if (req.body.title === undefined || req.body.text === undefined) {
+        res.status(500).send({});
+    } else {
+        data['news'].push({"title": req.body.title, "text": req.body.text, "date": Date()});
+        res.status(202).send({"title": req.body.title, "text": req.body.text, "date": Date()});
+    }
 });
 
 app.get('/schedule', (req, res) => {
