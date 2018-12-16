9e09a1c20caddd503b79a7d6754d34091cd4612f
f0s3 <power.aerozoll@gmail.com>
2018-12-16 21:17:37 +0200

    Add cors module

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors')();
+
 const data = require('./data');
+const baseUrl = '/api/';
+const port = 4201;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors);
 
-app.get('/', (req, res) => {
+app.get(`${baseUrl}`, (req, res) => {
     res.send({news: data['news']});
 });
 
-app.get('/news', (req, res) => {
+app.get(`${baseUrl}news`, (req, res) => {
     res.send({news: data['news']});
 });
 
-app.post('/news', (req, res) => {
+app.post(`${baseUrl}news`, (req, res) => {
     if (req.body.title === undefined || req.body.text === undefined) {
         res.status(500).send({});
     } else {
@@ -22,16 +27,16 @@ app.post('/news', (req, res) => {
     }
 });
 
-app.get('/schedule', (req, res) => {
+app.get(`${baseUrl}schedule`, (req, res) => {
     res.send({news: data['schedule']});
 });
 
-app.get('/teachers', (req, res) => {
+app.get(`${baseUrl}teachers`, (req, res) => {
     res.send({news: data['teachers']});
 });
 
-app.get('/books', (req, res) => {
+app.get(`${baseUrl}books`, (req, res) => {
     res.send({news: data['books']});
 });
 
-app.listen(4201, () => { console.log('application is running on port 4201.') });
+app.listen(port, () => { console.log(`application is running on port ${port}.`) });
