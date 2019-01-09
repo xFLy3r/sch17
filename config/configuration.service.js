988721987bf16253a384f07c753cf652ded0f5ce
f0s3 <power.aerozoll@gmail.com>
2019-01-09 22:07:21 +0200

    Remove unused config file

--- config/configuration.service.js
+++ config/configuration.service.js
@@ -1,11 +1,9 @@
 const application = require('./application');
 const admin = require('./admin');
 const routes = require('./routes');
-const db = require('./db');
 
 module.exports = {
     application,
     admin,
-    routes,
-    db
+    routes
 }
